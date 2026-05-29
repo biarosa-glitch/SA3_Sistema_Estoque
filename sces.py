d9fa07ffe65cafe68a8a45c319ba1b1b817d9886
biarosa-glitch <bianca.rosa@edu.senai.br>
2026-05-29 13:44:44 -0300

    Estrutura inicial do SCES

---
+++ sces.py
@@ -0,0 +1,19 @@
+produtos = []
+
+def adicionar_produto():
+    IDproduto = input(int("Qual o ID do produto?: "))
+    NomeProduto = input("Qual o nome do produto?: ")
+    quantidadeProduto = input(int("Qual a quantidade em estoque do produto?: "))
+    localizaçãoProduto = input("Qual a localização do produto?: ")
+    
+
+def listagem_produtos():
+    for linha in produtos:
+        print(linha)
+
+def buscar_produto():
+    print("")
+
+def atualizar_estoque():
+    print("")
+
