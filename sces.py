a81492c1ff93f2df6a754678564de5f7c1366a1e
biarosa-glitch <bianca.rosa@edu.senai.br>
2026-06-18 11:10:25 -0300

    Aprimorei a função de buscar produto e a de atualizar o estoque. Todas as funções estão aprimoradas (obrigada Gersão e Bruno).

--- sces.py
+++ sces.py
@@ -1,7 +1,10 @@
-produtos = []
+produtos = [
+    [123, "Chave", 300, "A-190"]
+]
 
 def adicionar_produto():
     global IDproduto
+    global NomeProduto
     IDproduto = int(input("Qual o ID do produto?: "))
     NomeProduto = input("Qual o nome do produto?: ")
     quantidadeProduto = int(input("Qual a quantidade em estoque do produto?: "))
@@ -14,16 +17,31 @@ def listagem_produtos():
         print(linha)
 
 def buscar_produto():
-    input("Qual o ID do produto procurado?: ")
-    if IDproduto == True:
-        for linha in produtos:
-            print(linha)
-    else:
-        print("Esse produto não existe!")
+    IDProcurado = int(input("Qual o ID do produto procurado?: "))
+    produtoEncontrado = -1
+    for linha in produtos:
+        if IDProcurado == linha[0]:
+            print(f"Produto encontrado! {linha}")
+            produtoEncontrado = 1
+        if produtoEncontrado == -1:
+            print("Esse produto não existe! Digite um ID existente.")
 
 def atualizar_estoque():
-    print("")
+    ID_desejado= int(input("Qual o ID do produto que deseja alterar a quantidade?: "))
+    linha_produto = -1
+    for i in range(len(produtos)):
+        if ID_desejado == produtos[i][0]:
+            linha_produto = i
+    if linha_produto == -1:
+        print("Produto não encontrado! Tente novamente.")
+    else:
+        Alteracao_qte = int(input("Qual a nova quantidade?: "))
+        produtos[linha_produto][2] = Alteracao_qte
+
+
 
 adicionar_produto()
 listagem_produtos()
 buscar_produto()
+atualizar_estoque()
+listagem_produtos()
